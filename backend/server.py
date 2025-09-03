163d7cc20b9f444897c5bcf818ddc4bdadce4e69
Ryunaja <Kawiwat.ryu@gmail.com>
2025-09-03 23:20:30 +0700

    add better route

--- backend/server.py
+++ backend/server.py
@@ -19,6 +19,26 @@ async def live():
     return StreamingResponse(streaming(), media_type="text/event-stream")
 
 
-@app.get("/session")
-async def session(year: int|None=None, gp: int|str|None=None, racetype: str|None=None, data: str|None=None):
-    return
+@app.get("/session/laptimes")
+async def session_laptimes(year: int|None=None, gp: int|str|None=None, session: str|None=None):
+    pass
+
+
+@app.get("/session/weatherdata")
+async def session_weatherdata(year: int|None=None, gp: int|str|None=None, session: str|None=None):
+    pass
+
+
+@app.get("/session/results")
+async def session_results(year: int|None=None, gp: int|str|None=None, session: str|None=None):
+    pass
+
+
+@app.get("/session/gaptime")
+async def session_gaptime(year: int|None=None, gp: int|str|None=None, session: str|None=None):
+    pass
+
+
+@app.get("/session/info")
+async def session_info(year: int|None=None, gp: int|str|None=None, session: str|None=None):
+    pass
