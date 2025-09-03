36bfaa19aeeb40520ae84b5f14ffba4af3f6c8e2
Ryunaja <Kawiwat.ryu@gmail.com>
2025-09-03 09:18:29 +0700

    add api route

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import asyncio
+import bin.Fetchpastrace
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"root" : "root"}
+async def streaming():
+    couter = 0
+    while True:
+        await asyncio.sleep(1)
+        yield f"tem {couter}"
+        couter += 1
+
+
+@app.get("/live")
+async def live():
+    return StreamingResponse(streaming(), media_type="text/event-stream")
+
+
+@app.get("/session")
+async def session(year: int=2025, gp: int|str=1, racetype: str="R", data: str="Laptime"):
+    return
