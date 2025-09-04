3cc9019050625c9af7490457e3dc87be7ff73023
Ryunaja <Kawiwat.ryu@gmail.com>
2025-09-04 23:48:37 +0700

    Add past race lookup api
    might not work lmao

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+from bin.Fetchpastrace import get_session_data, laptime_key, weather_key, results_key, info_key
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 import asyncio
@@ -20,25 +21,20 @@ async def live():
 
 
 @app.get("/session/laptimes")
-async def session_laptimes(year: int|None=None, gp: int|str|None=None, session: str|None=None):
-    pass
+async def session_laptimes(year: int=2025, gp: int|str=1, session: str="r"):
+    return get_session_data(year, gp, session, laptime_key)
 
 
 @app.get("/session/weatherdata")
-async def session_weatherdata(year: int|None=None, gp: int|str|None=None, session: str|None=None):
-    pass
+async def session_weatherdata(year: int=2025, gp: int|str=1, session: str="r"):
+    return get_session_data(year, gp, session, weather_key)
 
 
 @app.get("/session/results")
-async def session_results(year: int|None=None, gp: int|str|None=None, session: str|None=None):
-    pass
-
-
-@app.get("/session/gaptime")
-async def session_gaptime(year: int|None=None, gp: int|str|None=None, session: str|None=None):
-    pass
+async def session_results(year: int=2025, gp: int|str=1, session: str="r"):
+    return get_session_data(year, gp, session, results_key)
 
 
 @app.get("/session/info")
-async def session_info(year: int|None=None, gp: int|str|None=None, session: str|None=None):
-    pass
+async def session_info(year: int=2025, gp: int|str=1, session: str="r"):
+    return get_session_data(year, gp, session, info_key)
