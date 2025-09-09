3995f497fe18de701aa71874b8a7233c4d8ebe64
AungS8430 <aungshome@outlook.com>
2025-09-09 19:33:51 +0700

    Added CORS middleware

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,24 @@
 from util.Fetchpastrace import get_session_data
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 async def streaming():
